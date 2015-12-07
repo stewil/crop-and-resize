4516c4248f2d31f506ed3192e40350514a7a5eef
Steve Wilcox <steve.wilcox@outlook.com.au>
2015-12-08 08:26:50 +1100

    Added unsubscribe method to the events queue.

--- src/classes/events-queue.js
+++ src/classes/events-queue.js
@@ -6,47 +6,69 @@
         var _queue = [];
 
         return {
-            subscribe:subscribe,
-            unSubscribe:unSubscribe,
-            removeAll:removeAll
+            subscribe   :subscribe,
+            removeAll   :removeAll
         };
 
 
-        function subscribe(eventName, element, fn){
+        function subscribe(eventName, element, subscriberFn){
+
+            var knownQueueItem;
+
             for(var i = 0; i < _queue.length; i++){
                 if(_queue[i].event === event && _queue[i].element === element){
-                    _queue[i].events.push(fn);
-                    return;
+                    _queue[i].events.push(subscriberFn);
+                    knownQueueItem = _queue[i];
+                    break;
                 }
             }
-            new EventObject(eventName, element, fn);
+            return (knownQueueItem || new EventObject(eventName, element, subscriberFn))['publicEvents'];
         }
 
-        function EventObject(eventName, element, fn){
+        function EventObject(eventName, element, subscriberFn){
 
             var queueObject = {
-                events:[fn],
+                events:[subscriberFn],
                 element:element,
-                fn:fn
+                fn:subscriberFn,
+                publicEvents:{
+                    unSubscribe:unSubscribe
+                }
             };
 
             _queue.push(queueObject);
 
             element.addEventListener(eventName, onEvent);
 
+            return queueObject;
+
             function onEvent(e){
                 for(var i = 0; i < queueObject.events.length; i++){
                     queueObject.events[i].call(this, e);
                 }
             }
-        }
 
-        function unSubscribe(eventName, element, fn){
+            function unSubscribe(){
 
+                var functionIdx = queueObject.events.indexOf(subscriberFn);
+
+                if(functionIdx > -1 && queueObject){
+                    queueObject.events.splice(functionIdx, 1);
+                }
+
+                if(queueObject.events.length <= 0){
+                    element.removeEventListener(eventName, subscriberFn);
+                    _queue.splice(_queue.indexOf(queueObject), 1);
+                }
+            }
         }
 
         function removeAll(){
-
+            var l = _queue.length;
+            while(l--){
+                _queue[l].events = [];
+                _queue[l].publicEvents.unSubscribe();
+            }
         }
 
     }
